bae547c6697ce4705eecdbde06df63a9000de5ae
Joe Warren <themightyjoew@gmail.com>
2017-02-05 12:03:45 -0600

    Minutes Converter change
    Trying to make first commit to Minutes Converter in Github desktop
    version

---
+++ minuteConverter.js
@@ -0,0 +1,15 @@
+/* Minutes to hour equation
+Minutes * (1hr/60min) OR simply Minutes / 60 ie. 160minutes/60 = 2.67 hours 
+
+Mintes/60
+
+Multiply by 60 to get back to minutes 
+
+ */
+
+var convertMinutes=function(n) {
+    var minutes = n/60
+
+    console.log(minutes)
+}
+convertMinutes(120) //How do we make userInput instead of what I enter here? userInput didn't work.
