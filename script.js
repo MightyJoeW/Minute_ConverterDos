0141b5a1bfdcbba3e2731ea5c025ea9afcfa7b2a
Joe Warren <joe.warren7@protonmail.com>
2019-10-26 16:33:43 -0500

    refactor: use es6

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-/* Minutes to hour equation
-Minutes * (1hr/60min) OR simply Minutes / 60 ie. 160minutes/60 = 2.67 hours 
+const outputHours = () => {
+	const minutes = document.getElementById("userInput").value;
+	const hours = minutes / 60;
 
-Mintes/60
-
-Multiply by 60 to get back to minutes 
-
-*/
-
-var convertMinutes=function(n) {
-	var minutes = n/60
-
-	console.log(minutes)
+	!isNaN(minutes)
+		? alert(`${minutes} minutes = ${hours} hour${minutes === '60' ? '' : 's'}`)
+		: alert('Invalid input. Please try again')
 }
-convertMinutes(120) //How do we make userInput instead of what I enter here? userInput didn't work.
